8ec6b0c7c49fb9c304160e0c20a43067ff7f84e2
elMuso <42069617+elMuso@users.noreply.github.com>
2021-12-29 17:07:44 -0400

    Refactor

--- src/main/java/core/Upscale.java
+++ src/main/java/core/Upscale.java
@@ -1,11 +1,13 @@
 package core;
 
+import java.awt.Color;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_dnn_superres.DnnSuperResImpl;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import javax.swing.ImageIcon;
 
 @SuppressWarnings({"java:S106","java:S2093"})
@@ -13,8 +15,9 @@ public class Upscale {
     private Upscale(){}
 
     public static boolean run(String loadPath, String savePath) {
+        Utils.write("aypaco",null);
         String[] mode = MainApp.getMode();
-        MainApp.write("Started Upscale Process ["+mode[0]+"]", null);
+        Utils.write("Started Upscale Process ["+mode[0]+"]", null);
         ImageIcon s = new ImageIcon(loadPath);
         int width = s.getIconWidth(); 
         int height = s.getIconHeight();
@@ -23,14 +26,14 @@ public class Upscale {
         height *= Integer.valueOf(mode[2]);
         String newSize = "["+width+"x"+height+"]";
         if(width > 6666 || height > 6666) {
-            MainApp.write("ERROR: Expected output has a side thats bigger than 6666 pixels", MainApp.SCARLET);
+            Utils.write("ERROR: Expected output has a side thats bigger than 6666 pixels", Utils.SCARLET);
             return false;
         }
 
 
         //should never happen
         if (mode[0].equals("ERROR")) {
-            MainApp.write("Error Loading Config", MainApp.SCARLET);
+            Utils.write("Error Loading Config", Utils.SCARLET);
             return false;
         }
 
@@ -41,42 +44,44 @@ public class Upscale {
         }
 
 
-        MainApp.write("Loading Image",null);
+        Utils.write("Loading Image",null);
         Mat image = imread(loadPath);
         if (image.empty()) {
-            MainApp.write("Error Loading Image",MainApp.SCARLET);
+            Utils.write("Error Loading Image",Utils.SCARLET);
             return false;
         }
         String modelName = "Models/"+mode[0]+".pb";
         Mat imageNew = new Mat();
-        MainApp.write("Loading AI",null);
+        Utils.write("Loading AI",null);
         DnnSuperResImpl sr = null;
             try {
                 sr = new DnnSuperResImpl();
                    File modelPath = new File(new File(Upscale.class.getProtectionDomain().getCodeSource().getLocation()
                         .toURI().getPath()).getParent()+File.separator+modelName);
                     if (!modelPath.exists()) {
-                        MainApp.write("Model not found!",MainApp.SCARLET);
+                        Utils.write("Model not found!",Utils.SCARLET);
                         return false;
                     }
-                MainApp.write("Trying to read model from "+modelPath,null);
+                Utils.write("Trying to read model from "+modelPath,null);
                 sr.readModel(modelPath.toString());
                 sr.setModel(mode[1], Integer.valueOf(mode[2]));
-                MainApp.write("Algorithm and Size Checked"+"\n \t Starting conversion",null);
+                Utils.write("""
+                            Algorithm and Size Checked
+                             \t Starting conversion""",null);
                 sr.upsample(image, imageNew);
                 
                 if(imageNew.isNull()){
-                    MainApp.write("Error UpScaling !",MainApp.SCARLET);
+                    Utils.write("Error UpScaling !",Utils.SCARLET);
                     return false;
                 }          
-                MainApp.write("Image was successfully upScaled from "+originalSize+"x"+mode[2]+", to "+newSize+"and saved to:",null);
-                MainApp.write(savePath,MainApp.SVGBLUE);
+                Utils.write("Image was successfully upScaled from "+originalSize+"x"+mode[2]+", to "+newSize+"and saved to:",null);
+                Utils.write(savePath,Utils.SVGBLUE);
                 Config.FIELD02.setValue(mode[0]);
                 imwrite(savePath, imageNew);
                 return true;
-            } catch(Exception e) {
-                MainApp.write("Error UpScaling !",MainApp.SCARLET);
-                e.printStackTrace();
+            } catch(NumberFormatException | URISyntaxException e) {
+                Utils.write("Error UpScaling !",Utils.SCARLET);
+                Utils.write(e.getMessage(), Color.yellow);
                 return false;
             }
                 finally {
@@ -87,4 +92,3 @@ public class Upscale {
         
     }
 }
-
